878a87f74f3842b967922f5340a0094f0c3214f7
Andres Echeverria <andres@brandpro.cl>
2022-05-06 16:23:14 -0400

    new suscribe

--- src/Componentes/HomePage/HomePage.js
+++ src/Componentes/HomePage/HomePage.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import NavBar from '../NavBar/NavBar'
 import Footer from '../Footer/Footer'
+import SuscribeMail from '../SuscribeEmail/SuscribeMail'
 import './HomePage.css'
 
 function HomePage() {
   return (
     <div className='homePageContainer'>
         <NavBar/>
-          <section className='homePage--mainBanner'>
-            Banner Principal
-          </section>
-          <section className='homePage--body'>
-            Cuerpo del home
-          </section>
+        <section className='homePage--mainBanner'>
+          Banner Principal
+        </section>
+        <main className='homePage--body'>
+          Cuerpo del home
+        </main>
+        <SuscribeMail/>
         <Footer/>
     </div>
 
