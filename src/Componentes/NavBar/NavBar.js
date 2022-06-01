9c70519fca77b2fa1d8d9060fe5a0459d111f866
Andres Echeverria <andres@brandpro.cl>
2022-06-01 14:02:47 -0400

    new navbar

--- src/Componentes/NavBar/NavBar.js
+++ src/Componentes/NavBar/NavBar.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 import logo from '../Logo/LogoHorizBN.png'
-import Cart from './Cart'
-import UserControls from './UserControls'
 import categoriasSitio from '../../Metasite/Categorias'
 import './NavBar.css'
 
@@ -68,12 +66,23 @@ function NavBar() {
 
           {/* User Controls */}
           <div className='col-sm-1 col-md-1	col-lg-1 col-xl-1	col-xxl-1 contenedorNav--userControls'>
-            <div className='userControls--sesion'><NavLink to='/login'>Iniciar sesión</NavLink></div>
+            <div className='userControls--sesion'><NavLink to='/login'>Iniciar sesión<div className='userControls--sesionIcon'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
+              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+              </svg>
+            </div></NavLink>
+            </div>
             <div className='userControls--resgistrarse'><NavLink to='/crear-cuenta'>Crear cuenta</NavLink></div>
           </div>
 
           {/* Cart Controls */}
-          <div className='col-sm-1	col-md-1	col-lg-1 col-xl-1	col-xxl-1 contenedorNav--cartControls'><Cart/></div>
+          <div className='col-sm-1	col-md-1	col-lg-1 col-xl-1	col-xxl-1 contenedorNav--cartControls'>
+            <div className='cartControls--container'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" className="bi bi-cart3 cart cartControls--cartIcon" viewBox="0 0 16 16">
+                <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+              </svg>
+              <div className='cartControls--qTip'>23</div>
+            </div>
+          </div>
 
       </nav>
     </>  
