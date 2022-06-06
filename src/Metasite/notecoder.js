2d18985c853c83a7f5e0172f7649bdbceacbd711
Andres Echeverria <andres@brandpro.cl>
2022-06-06 11:19:39 -0400

    navbar new buttons

--- src/Metasite/notecoder.js
+++
@@ -1,3 +0,0 @@
-const navbarCategorias = categoriasSitio.map((categorias) => <NavLink to ={categorias[0]}><NavDropdown.Item href=''>{categorias[listsubs]}</NavDropdown.Item></NavLink>)
-const listsubs = categorias.length
-
