d29153796ba258f1bd0e8cc6886cab45f504b828
Andres Echeverria <andres@brandpro.cl>
2022-06-15 17:44:23 -0400

    visible percent ok

--- src/Componentes/ProductCard/ProductCard.js
+++ src/Componentes/ProductCard/ProductCard.js
@@ -1,19 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './ProductCard.css'
 
 function ProductCard({producto}) {
   const discountPercentage = Math.round(((producto.price-producto.beforePrice)/producto.beforePrice)*100);
-  const [visible, setVisible] = useState(true)
+  const [visiblePercentage, setVisiblePercentage] = useState(true)
+
+  useEffect(() => {
+    if(producto.beforePrice === 0){
+      setVisiblePercentage(false);
+    } 
+  }, [discountPercentage])
+  
 
   return (
     < div className='productCard'>
             <div className='productCardImageContainer'><img className='productCardImage' src={producto.image} alt='' /></div>
             <div class="productCardBody">
               <div className='bodyPrices'>
-                <p className='bodyBeforePrice'>$ {(producto.beforePrice).toLocaleString('es-CL')}</p>
+                <p className='bodyBeforePrice'>{visiblePercentage === true ? `$${(producto.beforePrice).toLocaleString('es-CL')}` : ''}</p>
                 <div className='bodyPriceContainer'>
                   <h6 className='bodyPrice'><b>$ {(producto.price).toLocaleString('es-CL')}</b></h6>
-                  <h6 className="bodyDiscount">{discountPercentage}%</h6>
+                  <h6 className="bodyDiscount">{visiblePercentage === true ? `${discountPercentage}%` : '' }</h6>
                 </div>
               </div>
               <h6 className='cardTitle'>{producto.name}</h6>
